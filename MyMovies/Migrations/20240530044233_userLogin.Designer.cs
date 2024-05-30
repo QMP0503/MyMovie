b2c40a8c7985bc0ffda44acf7ac0d4726932566b
QMP0503 <quangypham0503@gmail.com>
2024-05-30 11:55:42 +0700

    Buid migration fixed. UserLogin db and migration have been added

--- MyMovies/Migrations/20240530044233_userLogin.Designer.cs
+++ MyMovies/Migrations/20240530044233_userLogin.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyMovies.Data;
 
@@ -11,9 +12,11 @@ using MyMovies.Data;
 namespace MyMovies.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240530044233_userLogin")]
+    partial class userLogin
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
